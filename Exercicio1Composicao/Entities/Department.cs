e57145c4cb59730ece00cb34424b7ddbe5bf9baf
Jorge <jorgearthurs@hotmail.com>
2021-03-08 00:27:21 -0300

    Exercicio Composicao1

---
+++ Exercicio1Composicao/Entities/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio1Composicao
+{
+    class Department
+    {
+        public string Name { get; set; }
+        
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+
+    
+}
